96f5a73a4a49fd6cb743fe3bec3d3e69ec4de5d4
Bryce Wray <bwray@wraytx.com>
2019-10-08 20:48:07 -0500

    Last config changes tonight

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `brycewray.com`, // was Gatsby Default Starter
+    description: `brycewray.com — Opinions, observations, nerdiness.`,
+    author: `@BryceWrayTX`,
   },
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -13,18 +15,28 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {},
+      plugins: [],
+    },
+    {
+      resolve: `gatsby-plugin-manifest`, 
+      options: {
+        name: `brycewray.com`, // was gatsby-starter-default
+        short_name: `brycewray.com`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0000d1`,
+        theme_color: `#0000d1`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-512x512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
