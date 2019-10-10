b8b88e37c63a5911a92063bd9eaeeaaccd0668a0
Bryce Wray <bwray@wraytx.com>
2019-10-09 20:39:37 -0500

    Good stopping point - fluid images working

--- src/templates/singlepost.js
+++ src/templates/singlepost.js
@@ -1,25 +1,35 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Image from "gatsby-image"
 
-function BlogPostTemplate(props) {
-  const post = props.data.markdownRemark
+export default ({ data }) => {
+  const post = data.markdownRemark
+  let featuredImageFluid = post.frontmatter.featured_image.childImageSharp.fluid
   return (
-    <div>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <Image fluid={featuredImageFluid} />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
   )
 }
 
-export default BlogPostTemplate
-
-export const pageQuery = graphql`
+export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
       frontmatter {
-        title
+        featured_image {
+          childImageSharp {
+            fluid(maxWidth: 1280) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
+      html
     }
   }
 `
