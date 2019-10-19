de23329ef10005141f84915b6dea10b27ec68e65
Bryce Wray <bwray@wraytx.com>
2019-10-18 21:12:29 -0500

    Essentially reverting previous edit that didn't get committed

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 900,
+              maxWidth: 900, 
            },
          },
          {
