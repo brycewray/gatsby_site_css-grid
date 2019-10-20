851e7a1c35ebb28ccb8e7942bbea5a2af8074de4
Bryce Wray <bwray@wraytx.com>
2019-10-20 10:41:13 -0500

    Corrected how the "About" featured_image appears (blur, not SVG-ishness)

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -47,19 +47,8 @@ export const query = graphql`
       frontmatter {
         featured_image {
           childImageSharp {
-            fluid {
-              base64
-              tracedSVG
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
-              originalImg
-              originalName
-              presentationWidth
-              presentationHeight
+            fluid(maxWidth: 1280) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
