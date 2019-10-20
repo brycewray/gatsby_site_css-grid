c041ccfa993b19846dace8ac05a77be68248e61c
Bryce Wray <bwray@wraytx.com>
2019-10-20 11:34:32 -0500

    Trying to get gatsby-plugin-paginate to work.

--- src/components/postslist.js
+++ src/components/postslist.js
@@ -1,1 +1,36 @@
-// This is where our posts list component will be.
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
+import SEO from "./seo"
+import Layout from './layout'
+
+export default function Pages ({ pageContext, data }) {
+  const { posts, page, pagesSum, prevPath, nextPath } = pageContext
+  return (
+    <>
+    <Layout>
+    <SEO title = "Posts" />
+      <main>
+        <div className="container">
+          <h1 className="ctr topOfMain">Posts</h1>
+          <div className="post-line"></div>
+          <div className="container-narrower">
+            {posts.allMarkdownRemark.edges.node.map(({ eachPost }) => (
+              <p>{eachPost.title}</p>
+            ))}
+            <p>Just testing for now; will pretty it up once it works.</p>
+            <p><Link to={prevPath}>Previous</Link></p>
+            <p><Link to={nextPath}>Next</Link></p>
+            <p>Page {page} of {pagesSum}</p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+    </>
+  )
+}
+
+Pages.propTypes = {
+  pageContext: PropTypes.object,
+  data: PropTypes.object,
+}
