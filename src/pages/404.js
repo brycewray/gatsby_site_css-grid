5a004059a2fbd80e4e05f99a3156ffb8276804ae
Bryce Wray <bwray@wraytx.com>
2019-10-27 08:14:23 -0500

    Making the 404 page more like what I have now.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="ctr">Page not found</h1>
+    <div className="post-line"></div>
+    <p className="ctr">Sorry, that page isn&rsquo;t on this site.</p>
+    <p className="ctr" style={{ marginBottom: "10em" }}>Please use the navigation menu, above, to find another&nbsp;page.</p>
   </Layout>
 )
 
