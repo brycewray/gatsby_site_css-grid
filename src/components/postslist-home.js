d649699e5b6f04fc20901b6cc683be983692ea1a
Bryce Wray <bwray@wraytx.com>
2020-03-29 13:34:13 -0500

    Adding GraphQL aliases; changing to 7 posts listed

--- src/components/postslist-home.js
+++ src/components/postslist-home.js
@@ -5,12 +5,18 @@ const PostsHomeList = () => (
   <StaticQuery
     query={graphql`
       query {
-        allMarkdownRemark(filter: {frontmatter: {tags: {eq: "post"}}}, limit: 7, sort: {fields: frontmatter___date, order: DESC}) {
+        wholeThing: allMarkdownRemark(filter: {frontmatter: {tags: {eq: "post"}}}) {
+          pageInfo {
+            itemCount
+          }
+        }
+        onlySeven: allMarkdownRemark(filter: {frontmatter: {tags: {eq: "post"}}}, limit: 7, sort: {fields: frontmatter___date, order: DESC}) {
           edges {
             node {
               frontmatter {
                 title
-                date(formatString: "MMMM D, YYYY")
+                date(formatString: "MMMM D,YYYY")
+                lastmod(formatString: "MMMM D, YYYY")
                 description
               }
               fields {
@@ -23,14 +29,21 @@ const PostsHomeList = () => (
     `}
     render={data => 
       <div>
-        {data.allMarkdownRemark.edges.map(({ node }) =>(
+        {data.onlySeven.edges.map(({ node }) =>(
           <div>
             <h2 className="h5" style={{ marginBottom: "0" }}><Link to={ node.fields.slug}>{node.frontmatter.title}</Link></h2>
-            <time datetime={node.frontmatter.date} className="pokey text-muted text-sans-serif">{node.frontmatter.date}</time>
+            <p><strong><em>{node.frontmatter.subtitle}</em></strong></p>
+            <time datetime={node.frontmatter.date} className="pokey text-muted text-sans-serif">{node.frontmatter.date}
+                {node.frontmatter.lastmod && (
+                  <>
+                  &nbsp;&bull;&nbsp;Last modified {node.frontmatter.lastmod}
+                  </>
+                )}
+                </time>
             <p className="pokey text-body">{node.frontmatter.description}</p>
           </div>
         ))}
-        <p><strong>All posts</strong> <em>(listed five per page)</em></p>
+        <p><Link to= "/posts"><strong>All {data.wholeThing.pageInfo.itemCount} posts</strong></Link> <span className="pokey"><em>(listed five per page)</em></span></p>
       </div>
     }
   />
