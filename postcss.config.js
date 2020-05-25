f5911fe504cc0506949b30741c5dd85a34417610
Bryce Wray <bwray@wraytx.com>
2020-05-25 15:19:57 -0500

    Adding TWCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,13 @@
-const postcssPresetEnv = require('postcss-preset-env')
-const cssnano = require('cssnano')
-const cssvariables = require('postcss-css-variables')
-const postcssPartialImport = require ('postcss-partial-import')
+const postcssImport = require('postcss-import')
 const postcssNesting = require('postcss-nesting')
 
 module.exports = {
   plugins: [
-    require('postcss-preset-env'),
-    postcssPresetEnv({
-      stage: 0,
-      browsers: [
-        'last 2 versions',
-        'IE 11',
-      ],
-      features: {
-        'nesting-rules': true,
-        'custom-properties': true,
-      },
+    require('postcss-preset-env')({ stage: 1 }),
+    postcssImport({
+      root: './src/assets/css',
     }),
-    postcssPartialImport({
-      root: './src/assets/css/',
-    }),
-    cssvariables({}),
+    require('tailwindcss'),
     postcssNesting({}),
-    process.env.NODE_ENV === 'production' 
-    ? cssnano({ preset: 'default' })
-    : null,
   ],
 }
